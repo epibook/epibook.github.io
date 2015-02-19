20ce60fd45d7e1fabd4e5287594988017c726268
panda-34 <panda-34@users.noreply.github.com>
2015-02-19 14:18:08 +0300

    Create parity3.py

---
+++ solutions/python/parity3.py
@@ -0,0 +1,11 @@
+import parity1
+
+_PRECOMPUTED_PARITY = [parity1.parity(i) for i in range(1 << 16)]
+
+def parity(x):
+    word_size = 16
+    bit_mask = 0xFFFF
+    return (_PRECOMPUTED_PARITY[x >> (3 * word_size)] ^
+            _PRECOMPUTED_PARITY[(x >> (2 * word_size)) & bit_mask] ^
+            _PRECOMPUTED_PARITY[(x >> word_size) & bit_mask] ^
+            _PRECOMPUTED_PARITY[x & bit_mask])
