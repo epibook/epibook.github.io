47ebe4fd1a3d9c4bb7862df8922aaed63b6ab85f
Tsung-Hsien Lee <tsung.hsien.lee@gmail.com>
2015-03-02 23:14:45 -0800

    fix binary tree level order python

--- solutions/python/binary_tree_level_order.py
+++ solutions/python/binary_tree_level_order.py
@@ -1,4 +1,4 @@
-# Binary_tree_level_order.cpp 100b4adabfd008775520062bd407c3323ea646af
+# Binary_tree_level_order.cc 78d35a792c18735bcb8ad1dfb20b40d20fb1e472
 import collections
 from binary_tree_prototype import BinaryTreeNode
 
@@ -15,16 +15,15 @@ def print_binary_tree_depth_order(tree):
     while processing_nodes:
         curr = processing_nodes.popleft()
         num_nodes_current_level -= 1
-        if not curr:
-            continue
-        print(curr.data, end=' ')
-        # @exclude
-        one_line_result.append(curr.data)
-        # @include
+        if curr:
+            print(curr.data, end=' ')
+            # @exclude
+            one_line_result.append(curr.data)
+            # @include
 
-        # Defer the null checks to the null test above.
-        processing_nodes.append(curr.left)
-        processing_nodes.append(curr.right)
+            # Defer the null checks to the null test above.
+            processing_nodes.append(curr.left)
+            processing_nodes.append(curr.right)
         # Done with the nodes at the current depth.
         if num_nodes_current_level == 0:
             print()
@@ -40,17 +39,23 @@ def main():
     #      3
     #    2   5
     #  1    4 6
+    #10
+    # 13
     tree = BinaryTreeNode(3)
     tree.left = BinaryTreeNode(2)
     tree.left.left = BinaryTreeNode(1)
+    tree.left.left.left = BinaryTreeNode(10)
+    tree.left.left.left.right = BinaryTreeNode(13)
     tree.right = BinaryTreeNode(5)
     tree.right.left = BinaryTreeNode(4)
     tree.right.right = BinaryTreeNode(6)
     # should output 3
     #               2 5
     #               1 4 6
+    #               10
+    #               13
     print_binary_tree_depth_order(tree)
-    assert results == [[3], [2, 5], [1, 4, 6]]
+    assert results == [[3], [2, 5], [1, 4, 6], [10], [13], []]
 
 
 if __name__ == '__main__':
